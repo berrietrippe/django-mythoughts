3a8919a1b1b27e29788173ccdb1eef628d0f6404
Berrie Trippe <berrietrippe@gmail.com>
2017-01-09 20:31:26 +0100

    updated settings

--- mythoughts/settings.py
+++ mythoughts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8p===g0$%ouweapwa93b%@)y2lh($b5dinm%^ql(ew%vi26f62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', '.herokuapp.com']
 
